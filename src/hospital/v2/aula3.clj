8dd9bb2efc56f41eedc27c7155f7308798ebf62d
Cesar Alcancio <cesar.alcancio@payclip.com>
2020-11-20 21:11:33 -0300

    aula 3

--- src/hospital/v2/aula3.clj
+++ src/hospital/v2/aula3.clj
@@ -4,8 +4,39 @@
 
 (defn carrega-paciente [id]
   (println "Carregando" id)
-  (Thread/sleep 2000)
+  (Thread/sleep 1000)
   {:id id, :carregado-em (h.v2.logica/agora)})
 
-(pprint (carrega-paciente 15))
-(pprint (carrega-paciente 30))
+;(pprint (carrega-paciente 15))
+;(pprint (carrega-paciente 30))
+
+
+(defn carrega-se-na-existe
+  [pacientes id carregadora]
+  (if (contains? pacientes id)
+    pacientes
+    (let [paciente (carregadora id)]
+      (assoc pacientes id paciente))))
+
+;(pprint (carrega-se-na-existe {} 15 carrega-paciente))
+;(pprint (carrega-se-na-existe {15 {:id 15}} 15 carrega-paciente))
+
+(defprotocol Carregavel
+  (carrega! [this id]))
+
+(defrecord Cache
+  [cache carregadora]
+
+  Carregavel
+  (carrega! [this id]
+    (swap! cache carrega-se-na-existe id carregadora)
+    (get @cache id)))
+
+(def pacientes (->Cache (atom {}), carrega-paciente))
+
+; treinar un pouco de atom pra relembrar
+(let [pessoa (atom {:nome "Cesar"})]
+  (pprint (:nome @pessoa))
+  (swap! pessoa assoc :nome "Jose")
+  (pprint (:nome @pessoa)))
+
